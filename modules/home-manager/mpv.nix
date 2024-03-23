720eb4ddf830cea9b779ab060c5bd1ae3cfef29c
Benjamin McIntyre <benjaminmcintyre06@protonmail.com>
2024-03-23 11:27:36 -0400

    Extra mpv config

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -1,4 +1,4 @@
-{config, pkgs, inputs, ...}:
+{config, lib, pkgs, inputs, ...}:
 {
   programs.mpv = {
     enable = true;
@@ -8,14 +8,19 @@
       thumbnail
       quality-menu
       reload
+      webtorrent-mpv-hook
     ];
     config = {
       osc = "no";
+      slang = "en";
+      alang = "jp,en";
+      profile = lib.mkDefault "high-quality";
+      force-window="immediate";
     };
     scriptOpts = {
       sponsorblock = {
-        categories="music_offtopic";
-        skip_categories="music_offtopic";
+        categories="music_offtopic,intro,outro,interaction,selfpromo";
+        skip_categories="music_offtopic,sponsor,selfpromo,outro";
       };
     };
   };
