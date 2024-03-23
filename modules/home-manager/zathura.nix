0fd072197f9cad5def5ebfc2c7064377b964aa5a
Benjamin McIntyre <benjaminmcintyre06@protonmail.com>
2024-03-23 11:26:42 -0400

    Fix zathura not actually being default for pdfs

--- modules/home-manager/zathura.nix
+++ modules/home-manager/zathura.nix
@@ -1,7 +1,7 @@
 {pkgs, config, ...}:
 {
   xdg.mimeApps.defaultApplications = {
-    "application/pdf" = "zathura.desktop";
+    "application/pdf" = "org.pwmt.zathura.desktop";
   };
   programs.zathura = {
     enable = true;
