9ee4c30ada87398e072629fd7a8b9ca84a7db93e
Benjamin McIntyre <benjaminmcintyre06@protonmail.com>
2025-01-27 06:52:46 -0500

    Add mokuro

--- modules/home-manager/japanese.nix
+++ modules/home-manager/japanese.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, inputs, ... }:
 {
-  home.packages = [ #
-    pkgs.noto-fonts-cjk-sans
+  home.packages = [
+    #
+    # pkgs.noto-fonts-cjk-sans
+    pkgs.mokuro
   ];
   i18n.inputMethod = {
     enabled = "fcitx5";
@@ -11,6 +13,10 @@
     ];
   };
   xdg.dataFile."fcitx5/themes".source = inputs.fcitx5-gruvbox;
+  # xdg.dataFile."fonts" = {
+  #   enable = true;
+  #   source = config.lib.file.mkOutOfStoreSymlink "/run/current-system/sw/share/X11/fonts";
+  # };
   # xdg.configFile."fcitx5/conf/classic-ui.config".text = ''
   #   Theme = Gruvbox-Dark
   #   '';
